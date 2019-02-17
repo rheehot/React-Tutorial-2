97316e1224ec7f30f16ed65361bb00e51e47972f
또르 <hansol416@naver.com>
2019-02-17 16:55:07 +0900

    Error Fix

--- server.js
+++ server.js
@@ -5,8 +5,32 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.get('/api/hello', (req,res)=>{
-    res.send({message:'Hello Express!'});
-});
+
+app.get('/api/customers', (req,res) => {
+    res.send([
+        {
+            'id' : 1,
+            'image':'https://placeimg.com/64/64/1', //placeimg 는 랜덤으로 이미지를 보여주는 사이트이고 64/64 는 사이즈 이다.
+            'name' : '최한솔',
+            'birthday' : '970531',
+            'gender' : '남자',
+            'job' : '대학생'
+          },{
+            'id' : 2,
+            'image':'https://placeimg.com/64/64/2', //placeimg 는 랜덤으로 이미지를 보여주는 사이트이고 64/64 는 사이즈 이다.
+            'name' : '김고은',
+            'birthday' : '960317',
+            'gender' : '여자',
+            'job' : '뉴스기자'
+          },{
+            'id' : 3,
+            'image':'https://placeimg.com/64/64/3', //placeimg 는 랜덤으로 이미지를 보여주는 사이트이고 64/64 는 사이즈 이다.
+            'name' : '김사또',
+            'birthday' : '910914',
+            'gender' : '남자',
+            'job' : '공무원'
+          }
+    ])
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
