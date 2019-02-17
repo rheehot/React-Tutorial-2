876b33605c85d0b325d1fb113c0b8042a5061caa
또르 <hansol416@naver.com>
2019-02-17 21:24:48 +0900

    Error Fix & Add Customer API Loading View

--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -10,7 +10,7 @@ class Customer extends React.Component{
         return(
             <TableRow>
                 <TableCell>{this.props.id}</TableCell>
-                <TableCell><img src={this.props.image}/></TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
                 <TableCell>{this.props.name}</TableCell>
                 <TableCell>{this.props.birthday}</TableCell>
                 <TableCell>{this.props.gender}</TableCell>
