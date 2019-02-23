b7c3d1eed5871e567eef7940e893442b5e38e2f8
또르 <hansol416@naver.com>
2019-02-23 18:52:19 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- client/src/App.js
+++ client/src/App.js
@@ -42,7 +42,7 @@ class App extends Component {
   }
 
   callApi = async () => {
-    const response = await fetch('api/customers');
+    const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
   }
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,38 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req,res) => {
-    res.send([
-        {
-            'id' : 1,
-            'image':'https://placeimg.com/64/64/1', //placeimg 는 랜덤으로 이미지를 보여주는 사이트이고 64/64 는 사이즈 이다.
-            'name' : '최한솔',
-            'birthday' : '970531',
-            'gender' : '남자',
-            'job' : '대학생'
-          },{
-            'id' : 2,
-            'image':'https://placeimg.com/64/64/2', //placeimg 는 랜덤으로 이미지를 보여주는 사이트이고 64/64 는 사이즈 이다.
-            'name' : '김고은',
-            'birthday' : '960317',
-            'gender' : '여자',
-            'job' : '뉴스기자'
-          },{
-            'id' : 3,
-            'image':'https://placeimg.com/64/64/3', //placeimg 는 랜덤으로 이미지를 보여주는 사이트이고 64/64 는 사이즈 이다.
-            'name' : '김사또',
-            'birthday' : '910914',
-            'gender' : '남자',
-            'job' : '공무원'
-          },{
-            'id' : 4,
-            'image':'https://placeimg.com/64/64/4', //placeimg 는 랜덤으로 이미지를 보여주는 사이트이고 64/64 는 사이즈 이다.
-            'name' : '김혜미',
-            'birthday' : '190830',
-            'gender' : '여자',
-            'job' : '백수'
-          }
-    ])
-})
+    connection.query(
+      "SELECT * FROM CUSTOMER",
+      (err,rows,fields) => {
+        res.send(rows);
+      }
+    );
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
